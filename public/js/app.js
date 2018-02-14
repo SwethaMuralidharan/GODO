665bd26d53f2a0bb44a5a682e099a815c586e4f9
juancarlucci <juancarloscollins@gmail.com>
2018-02-14 13:07:02 -0800

    get user input into js

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,36 @@
 //title
 // https/godos.herokuapp.com/godos
 
-$(document).on("ready", function(){
 
-  // let $date= $("date").value;
-  let $date= $( "#datepicker" ).datepicker();
-  let $duration = $("duration").value;
-  let $location = $("location").value;
-  console.log($date, $duratiuon, $location);
-})
 
+$(document).ready(function(){
+
+    console.log("conected");
+
+    let $duration = $("input[name=duration]").val();
+
+    $("form").submit(function(){
+          event.preventDefault();
+        console.log("submitted");
+      // document.getElementById("myForm").elements.namedItem("fname").value;
+      // let $date= $("date").val();
+
+      // let $date= $( "#datepicker" ).datepicker();
+      let $date = $("input[name=date]").val();
+      let $duration = $("input[name=duration]").val();
+      let $location = $("input[name=location]").val();
+      // let $location = $("location").val();
+      console.log($duration, $location, $date);
+
+     });
+    //  $("form").on('submit', function (){
+    //
+    //   // let $date= $("date").value;
+    //   let $date= $( "#datepicker" ).datepicker();
+    //   let $duration = $("duration").val();
+    //   let $location = $("location").val();
+    //   console.log($date, $duratiuon, $location);
+    // });
 
 
 
