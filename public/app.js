70e126edf8084fb3edf04479a9a41d70c1533c73
juancarlucci <juancarloscollins@gmail.com>
2018-02-14 11:51:17 -0800

    add basic js structure

---
+++ public/app.js
@@ -0,0 +1,17 @@
+
+//title
+// https/godos.herokuapp.com/godos
+
+$(document).on("ready", function(){
+
+  // let $date= $("date").value;
+  let $date= $( "#datepicker" ).datepicker();
+  let $duration = $("duration").value;
+  let $location = $("location").value;
+  console.log($date, $duratiuon, $location);
+})
+
+
+
+
+}); //end document ready
